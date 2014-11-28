d4e52f24b0d4d6a52a0760ae7b6e35ffdf662897
pldorrell <pldorrell@samsungLaptop>
2014-11-28 06:42:32 -0500

    modified

--- src/main/webapp/script/dashboard/app.js
+++ src/main/webapp/script/dashboard/app.js
@@ -1,15 +1,88 @@
-angular.module('HFWApp', [
-  'HFWApp.controllers','HFWApp.directives'
-]);
-/*        .
-  config(['$routeProvider', function($routeProvider) {
-  $routeProvider.
-      when('/dashboard', {templateUrl: '/HFW/script/app/partials/dashboard.html',   controller: "dashboardController"}).
-      otherwise({redirectTo: '/accounts'});
-}]);
-*/
-angular.module('HFWApp.directives', []).
-directive('modal', function () {
+var hfwApp = angular.module('HFWApp', []);
+
+hfwApp.controller('dashboardController', function ($scope,$http) {
+    $scope.accountFormData = {};
+    $scope.registryTransactions={}; 
+    $scope.registryTransactionFormData={}; 
+    $scope.selectedAccount = null;
+    $scope.showAccountModal = false;
+    $scope.showTransactionModal = false;
+    $http.get("/HFW/services/api/v1/accounts/search/all")
+    .success(function(response) {$scope.accounts = response;});    
+   
+    
+    $scope.clickGoButton = function(x) {
+        console.log(x.id);
+        this.selectedAccount = x;
+        $scope.registryTransactionFormData.primaryAccount=x.id;
+        $scope.$emit('showRegisterTransactions', x);
+        this.getTransactionsForAccount(x.id);
+    };
+
+    $scope.showNewAccount = function(x) {
+        $scope.showAccountModal=true;
+        //$("#accountDetailsForm").show();
+    };
+
+    $scope.clickNewAccountCancel = function(x) {
+        $scope.showAccountModal=false;
+        //$("#accountDetailsForm").hide();
+    };
+    
+    $scope.showNewTransaction = function(x) {
+        $scope.showTransactionModal=true;
+        //$("#transactionDetailsForm").show();
+    };
+    $scope.clickNewTransactionCancel=function(x) {
+        $scope.showTransactionModal=false;
+        //$("#transactionDetailsForm").hide();
+    }
+    
+
+    
+    $scope.addAccount = function() {
+	$http({
+        method  : 'POST',
+        url     : '/HFW/services/api/v1/accounts/save',
+        data    : JSON.stringify($scope.accountFormData),  // pass in data as strings
+        headers : { 'Content-Type': 'application/json'  }  // set the headers so angular passing info as form data (not request payload)
+            })
+        .success(function(data) {
+            
+            $scope.accounts.push(data);
+            
+            console.log(data);
+      
+        });
+    };
+    
+    
+    
+    $scope.getTransactionsForAccount = function(accountId) {
+        $http.get("/HFW/services/api/v1/register/get/all/"+accountId)
+    .success(function(response) {$scope.registryTransactions = response;}); 
+    };
+    
+    $scope.addRegistryTransaction = function() {
+        
+	$http({
+        method  : 'POST',
+        url     : '/HFW/services/api/v1/register/save',
+        data    : JSON.stringify($scope.registryTransactionFormData),  // pass in data as strings
+        headers : { 'Content-Type': 'application/json'  }  // set the headers so angular passing info as form data (not request payload)
+            })
+        .success(function(data) {
+            
+            $scope.registryTransactions.push(data);
+            
+            console.log(data);
+      
+        });
+    };
+    
+});
+
+hfwApp.directive('modal', function () {
     return {
       template: '<div class="modal fade">' + 
           '<div class="modal-dialog">' + 
@@ -50,3 +123,4 @@ directive('modal', function () {
       }
     };
   });
+
