590f7d32a39dd8d7862fd1eec5199cc5807d17e7
pldor <pldor@DESKTOP-E51M9NQ>
2016-11-27 11:15:04 -0500

    updates

--- src/main/java/com/hf/hfw/dao/lwmds/ConfigBuilder.java
+++ src/main/java/com/hf/hfw/dao/lwmds/ConfigBuilder.java
@@ -1,10 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.hf.hfw.dao.lwmds;
 
+import com.hf.hfw.application.ConfigurationDirectoryService;
 import com.hf.hfw.dao.lwmds.converter.AccountConverter;
 import com.hf.hfw.dao.lwmds.converter.BudgetConverter;
 import com.hf.hfw.dao.lwmds.converter.DailyBalanceConverter;
@@ -17,17 +13,28 @@ import com.hf.lwdatastore.CollectionDescription;
 import com.hf.lwdatastore.CollectionDescriptionImpl;
 import com.hf.lwdatastore.DataStoreConfig;
 import com.hf.lwdatastore.DataStoreConfigImpl;
+import java.io.File;
 
 /**
  *
  * @author pldorrell
  */
 public class ConfigBuilder {
-
+    public final static String COLLECTION_ACCOUNTS = "account";
+    public final static String COLLECTION_TRANSACTIONS = "transactions";
+    public final static String COLLECTION_ONLINE_TRANSACTIONS = "onlineTransactions";
+    public final static String COLLECTION_SCHEDULED_TRANSACTIONS = "scheduledTransactions";
+    public final static String COLLECTION_BUDGET = "budgets";
+    
+    private ConfigurationDirectoryService configurationDirectoryService;
+    public void setConfigurationDirectoryService(ConfigurationDirectoryService configurationDirectoryService){
+        this.configurationDirectoryService = configurationDirectoryService;
+    }
     public DataStoreConfig getConfig() {
         DataStoreConfigImpl dsConfig = new DataStoreConfigImpl();
 
-        dsConfig.setDataDir("/home/pldorrell/.hfw_app/data");
+        
+        dsConfig.setDataDir(this.configurationDirectoryService.getDataStorageDirectory() + File.separator+"lwds" );
 
         
         dsConfig.addCollection(buildAccountCollectionDescription());
@@ -43,7 +50,7 @@ public class ConfigBuilder {
     
     private CollectionDescription buildAccountCollectionDescription() {
         CollectionDescriptionImpl collectionDescriptionImpl = new CollectionDescriptionImpl();
-        collectionDescriptionImpl.setName("account");
+        collectionDescriptionImpl.setName(COLLECTION_ACCOUNTS);
         collectionDescriptionImpl.setIdAttribute("id");
         collectionDescriptionImpl.addIndexedAttribute("accountNumber");
         collectionDescriptionImpl.addIndexedAttribute("name");
@@ -58,7 +65,7 @@ public class ConfigBuilder {
     private CollectionDescription buildBudgetCollectionDescription() {
         CollectionDescriptionImpl collectionDescriptionImpl = new CollectionDescriptionImpl();
         collectionDescriptionImpl = new CollectionDescriptionImpl();
-        collectionDescriptionImpl.setName("budget");
+        collectionDescriptionImpl.setName(COLLECTION_BUDGET);
         collectionDescriptionImpl.setIdAttribute("id");        
         collectionDescriptionImpl.setConverter(new BudgetConverter());
         return collectionDescriptionImpl;
@@ -67,12 +74,13 @@ public class ConfigBuilder {
     private CollectionDescription buildTransactionCollectionDescription() {
         CollectionDescriptionImpl collectionDescriptionImpl = new CollectionDescriptionImpl();
         collectionDescriptionImpl = new CollectionDescriptionImpl();
-        collectionDescriptionImpl.setName("transactions");
+        collectionDescriptionImpl.setName(COLLECTION_TRANSACTIONS);
         collectionDescriptionImpl.setIdAttribute("id");        
         collectionDescriptionImpl.addIndexedAttribute("accountId");
         collectionDescriptionImpl.addIndexedAttribute("category");
         collectionDescriptionImpl.addIndexedAttribute("month");
         collectionDescriptionImpl.addIndexedAttribute("year");
+        collectionDescriptionImpl.addIndexedAttribute("statusTxt");
         collectionDescriptionImpl.setConverter(new TransactionConverter());
         return collectionDescriptionImpl;
     }
@@ -80,7 +88,7 @@ public class ConfigBuilder {
     private CollectionDescription buildOnlineTransactionCD() {
         CollectionDescriptionImpl collectionDescriptionImpl = new CollectionDescriptionImpl();
         collectionDescriptionImpl = new CollectionDescriptionImpl();
-        collectionDescriptionImpl.setName("onlineTransactions");
+        collectionDescriptionImpl.setName(COLLECTION_ONLINE_TRANSACTIONS);
         collectionDescriptionImpl.setIdAttribute("id");        
         collectionDescriptionImpl.addIndexedAttribute("accountId");
         collectionDescriptionImpl.setConverter(new OnlineTransactionConverter());
@@ -89,9 +97,13 @@ public class ConfigBuilder {
     private CollectionDescription buildScheduledTransactionCD() {
         CollectionDescriptionImpl collectionDescriptionImpl = new CollectionDescriptionImpl();
         collectionDescriptionImpl = new CollectionDescriptionImpl();
-        collectionDescriptionImpl.setName("scheduledTransactions");
+        collectionDescriptionImpl.setName(COLLECTION_SCHEDULED_TRANSACTIONS);
         collectionDescriptionImpl.setIdAttribute("id");        
         collectionDescriptionImpl.addIndexedAttribute("accountId");
+        collectionDescriptionImpl.addIndexedAttribute("month");
+        collectionDescriptionImpl.addIndexedAttribute("year");
+        collectionDescriptionImpl.addIndexedAttribute("original");
+        collectionDescriptionImpl.addIndexedAttribute("originalTransactionId");
         collectionDescriptionImpl.setConverter(new ScheduledTransactionConverter());
         return collectionDescriptionImpl;
     }    
